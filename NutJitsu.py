900a6799d7f947ce2859897d31eda8f09c0c8f2d
TallYate <66395059+TallYate@users.noreply.github.com>
2020-08-06 11:15:54 -0600

    Compatibility + eff
    Works on other people's computers
    
    Removed os.system("clear") because that is os dependent
    
    Wins only update when you win
    Belts only update when you win
    
    changed dict to variables

--- NutJitsu.py
+++ NutJitsu.py
@@ -2,26 +2,29 @@ import random, os, time
 
 #   Print Player Info  ________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
-f = open('C:/Users/warre/Desktop/Python/Card Jitsu/wins.txt','r')
-g = open('C:/Users/warre/Desktop/Python/Card Jitsu/belts.txt','r')
-e = open('C:/Users/warre/Desktop/Python/Card Jitsu/achievements.txt','r')
+
+winsFile = os.getcwd() + "/savedData/wins.txt"
+beltFile = os.getcwd() + "/savedData/belts.txt"
+achievementsFile = os.getcwd() + "/savedData/achievements.txt"
+f = open(winsFile, 'r')
+g = open(beltFile, 'r')
+e = open(achievementsFile, 'r')
 cool = f.read()
 cool2 = g.read()
 cool3 = e.read()
+e.close()
+f.close()
+g.close()
 print('You Have', cool, 'Wins')
 print('You Have The', cool2)
 print('Achievements:', cool3)
 
 #   Cool Win  ______________________________________________________________________________________________________________________________________________________________________________________________________
 
-file_dict = {}
-file_dict["wins"] = cool
-
-file_dict["belts"] = ""
-file_dict["achieve"] = ""
-e.close()
-f.close()
-g.close()
+fWins = cool
+fBelt = ""
+#   Unused
+fAchievements = ""
 
 #   Start Game  ________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
@@ -31,7 +34,6 @@ continue_ = input('Press Enter To Start Playing >>>  ')
 
 player_cards = []
 enemy_cards = []
-wins = 0
 
 #   Card Types  ________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
@@ -39,15 +41,6 @@ fire = 'Fire'
 snow = 'Snow'
 water = 'Water'
 all_cards = [fire, water, snow]
-
-#____________________________________________________________________________________________________________________________________________________________________________________
-
-flag = True
-
-os.system('clear')
-
-#____________________________________________________________________________________________________________________________________________________________________________________
-
 
 #   card should be fire, water, or snow
 #   0 = weak, 1 = strong, 2 = neither
@@ -57,7 +50,12 @@ class Card:
         self.type = type
     def compare(self, other):
         if self.type == other.type:
-            return 2
+            if self.power == other.power:
+                return 2
+            elif self.power > other.power:
+                return 1
+            else:  
+                return 0
         elif self.type == fire:
             if other.type == snow:
                 return 1
@@ -70,7 +68,7 @@ class Card:
             if other.type == fire:
                 return 1
             return 0
-        return 0
+        return 3
 
 power_range = range(1, 16)
 
@@ -90,155 +88,107 @@ def display_cards(cardArray):
 create_cards(player_cards)
 create_cards(enemy_cards)
 
-while flag:
+playing = True
+print('\n\n\n\n\n\n\n\n')
+
+while playing:
 
     os.system('clear')
     display_cards(player_cards)
 
-#   Get Player Input  ____________________________________________________________________________________________________________________________________________________________________________________
+#   Player Card  ____________________________________________________________________________________________________________________________________________________________________________________
 
-    chosen_index = -1
-    while chosen_index == -1:
-        player_input = input('What Card Do You Choose? >>>  ')
-        if player_input.isdigit():
-            if int(player_input) <= len(player_cards) and int(player_input) > 0:
-                chosen_index = int(player_input) -1
+    player_index = -1
+    while player_index == -1:
+        typedInput = input('What Card Do You Choose? >>>  ')
+        if typedInput.isdigit():
+            if int(typedInput) <= len(player_cards) and int(typedInput) > 0:
+                player_index = int(typedInput) -1
             else:
-                print('Invalid input')
+                print('Invalid typedInput')
         else:
-                print('Invalid input')
-    
-    card = player_cards[chosen_index]
-    
-#   Player Place Card  ____________________________________________________________________________________________________________________________________________________________________________________
+                print('Invalid typedInput')
+                
+    player_card = player_cards[player_index]
 
-    print()
-    print('You Placed A Power', card.power, card.type, 'Card')
-    print()
+    print('\nYou Placed A Power', player_card.power, player_card.type, 'Card\n')
 
-#   Enemy Input  ____________________________________________________________________________________________________________________________________________________________________________________
+#   Enemy Card  ____________________________________________________________________________________________________________________________________________________________________________________
 
     enemy_index = random.choice(range(0, len(enemy_cards)-1 ))
-
     enemy_card = enemy_cards[enemy_index]
 
-#   Enemy Place Card  ____________________________________________________________________________________________________________________________________________________________________________________
+    print('\nThe Enemy Placed A Power', enemy_card.power, enemy_card.type, 'Card\n')
 
-    print()
-    print('The Enemy Placed A Power', enemy_card.power, enemy_card.type, 'Card')
-    print()
-
-#   Player Beat Enemy Thing  ____________________________________________________________________________________________________________________________________________________________________________________    
-
-
-#   Player Water Beat Fire  ____________________________________________________________________________________________________________________________________________________________________________________
-
-    type = card.compare(enemy_card)
+    type = player_card.compare(enemy_card)
 
     if type==1:
-        print()
-        print('You Win!')
-        print()
+        print('\nYou Win!\n')
         del enemy_cards[enemy_index]
     elif type==0:
-        print()
-        print('You Lose!')
-        print()
-        del player_cards[chosen_index]
+        print('\nYou Lose!\n')
+        del player_cards[player_index]
     else:
-        if card.power == enemy_card.power:
-            print()
-            print("It's A Tie!")
-            print()
-        elif card.power > enemy_card.power:
-            print()
-            print('You Win!')
-            print()
-            del enemy_cards[enemy_index]
-        else:
-            print()
-            print('You Lose!')
-            print()
-            del player_cards[chosen_index]
-
-#   Print How Much Cards Player And Enemy Have  ____________________________________________________________________________________________________________________________________________________________________________________
-
+        print("\nIt's A Tie!\n")
+    
     print('You Have {} Cards'.format(len(player_cards)))
     print('Your Opponent Has {} Cards'.format(len(enemy_cards)))
 
-#   Detect If Player Wins Or Loses The Game  ____________________________________________________________________________________________________________________________________________________________________________________
+#   no cards = big L  ____________________________________________________________________________________________________________________________________________________________________________________
 
     if len(player_cards) == 0:
         print('You Lost :(')
-        flag = False
+        playing = False
 
     elif len(enemy_cards) == 0:
         print('You Won!!!')
-        flag = False
+        playing = False
+        #   Write Wins
+        wins = int(fWins) + 1
+        fWins = str(wins)
+        f = open(winsFile, 'w')
+        f.write(fWins)
+        f.close()
 
-#   Write Wins  ____________________________________________________________________________________________________________________________________________________________________________________
+        #   Belt Things ____________________________________________________________________________________________________________________________________________________________________________________
+        write = False
+        if int(wins) == 5:
+            fBelt = 'White Belt'
+            write = True
+        elif int(wins) == 13:
+            fBelt = 'Yellow Belt'
+            write = True
+        elif int(wins) == 21:
+            fBelt = 'Orange Belt'
+            write = True
+        elif int(wins) == 30:
+            fBelt = 'Green Belt'
+            write = True
+        elif int(wins) == 40:
+            fBelt = 'Blue Belt'
+            write = True
+        elif int(wins) == 52:
+            fBelt = 'Red Belt'
+            write = True
+        elif int(wins) == 64:
+            fBelt = 'Purple Belt'
+            write = True
+        elif int(wins) == 76:
+            fBelt = 'Coffee Belt'
+            write = True
+        elif int(wins) == 88:
+            fBelt = 'Black Belt'
+            write = True
 
-        wins = int(file_dict["wins"])
-        wins = wins + 1
-        file_dict["wins"] = wins
-
-#_______________________________________________________________________________________________________________________________________________________________________________________________________   
-
-    continue_ = input('Press Enter To Continue >>>  ')
-    os.system('clear')
-
-#   Write Wins  _____________________________________________________________________________________________________________________________________________________________________________________
-
-wins = file_dict['wins']
-f = open('C:/Users/warre/Desktop/Python/Card Jitsu/wins.txt', 'w')
-f.write(str(wins))
-f.close()
-
-#   Write Belts  ________________________________________________________________________________________________________________________________________________________________________
-
-belts = file_dict['belts']
-f = open('C:/Users/warre/Desktop/Python/Card Jitsu/belts.txt', 'w')
-f.write(str(belts))
-f.close()
-
-wins = int(file_dict["wins"])
-belt = file_dict['belts']
-
-#   Belt Things  ______________________________________________________________________________________________________________________________________________________________________________________
-
-if int(wins) == 5:
-    file_dict['belts'] = 'White Belt'
-    print('You Have Obtained The White Belt!!!!')
-
-elif int(wins) == 13:
-    file_dict['belts'] = 'Yellow Belt'
-    print('You Have Obtained The Yellow Belt!!!!')
-
-elif int(wins) == 21:
-    file_dict['belts'] = 'Orange Belt'
-    print('You Have Obtained The Orange Belt!!!!')
-
-elif int(wins) == 30:
-    file_dict['belts'] = 'Green Belt'
-    print('You Have Obtained The Green Belt!!!!')
-
-elif int(wins) == 40:
-    file_dict['belts'] = 'Blue Belt'
-    print('You Have Obtained The Blue Belt!!!!')
-
-elif int(wins) == 52:
-    file_dict['belts'] = 'Red Belt'
-    print('You Have Obtained The Red Belt!!!!')
-
-elif int(wins) == 64:
-    file_dict['belts'] = 'Purple Belt'
-    print('You Have Obtained The Purple Belt!!!!')
-
-elif int(wins) == 76:
-    file_dict['belts'] = 'Coffee Belt'
-    print('You Have Obtained The Coffee Belt!!!!')
+        if write:
+            if fBelt=='Black Belt':
+                print('You Have Obtained The Black Belt!!!!')
+                print('You Are Now A Card Jitsu Master!!!')
+            else:
+                print('You Have Obtained The ', fBelt, '!!!!')
+            f = open(beltFile, 'w')
+            f.write(fBelt)
+            f.close()
     
-elif int(wins) == 88:
-    file_dict['belts'] = 'Black Belt'
-    print('You Have Obtained The Black Belt!!!!')
-    print('You Are Now A Card Jitsu Master!!!')
+    continue_ = typedInput('Press Enter To Continue >>>  ')
+    print('\n\n\n\n\n\n\n\n')
